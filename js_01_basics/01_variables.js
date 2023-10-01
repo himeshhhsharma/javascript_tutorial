704bd46551425a1f65c7ad788f0cd1742cf51d1e
himeshhhsharma <107181195+himeshhhsharma@users.noreply.github.com>
2023-10-01 12:24:12 +0000

    container config done

---
+++ js_01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=2020522770;
+let accountEmail="itshimesh23@gmail.com";
+var accountPassword="1234";
+accountCity="Kota";
+// accountId=200101133;    not allowed
+// console.log(accountId);
+accountEmail="23himesh@gmail.com";
+accountPassword="2309";
+accountCity="Noida";
+let accountState;
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+/*var was used before ES6,now do not use var bcoz it gives issue in local and global scope.
+*/
